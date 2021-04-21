81bba5790a9fc9705c86b0ef0b31ede93aa13b4f
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-04-21 15:18:33 -0700

    type of queries

--- src/app.ts
+++ src/app.ts
@@ -1,67 +1,21 @@
-/* A “this” Keyword Primer */
+/* “typeof” Type Queries */
+typeof ''; // string
+typeof []; // object
 
-// function
-// function myFunction() {
-//   console.log('Function:::', this);
-// }
-
-// myFunction();
-
-// Object literal
-const myObj = {
-  myMethod() {
-    console.log('Object::::', this);
-  }
+const person = {
+  name: 'Todd',
+  age :27
 }
-// myObj.myMethod();
+type Person = typeof person;
 
-// Classes
-// class MyClass{
-//   MyMethod() {
-//     console.log('Class::::', this);
-//   }
-// }
-
-// const MyInstance = new MyClass();
-// MyInstance.MyMethod();
-
-/* Exploring “this” with .call, .apply and .bind */
-// continued
-// function myFunction(...text: string[]) {
-//   console.log('Function:::', this, text);
-// }
-
-// myFunction('ABC', 'DEF');
-// const bindFunction = myFunction.bind(myObj);
-// // bind will take initial function, bind context to it, then give it a new context on call
-// bindFunction('ABC', 'DEF');
-// bindFunction('123','456');
-// bindFunction('ABC', 'DEF');
-// myFunction.call(myObj, 'ABC', 'DEF'); // c for call and comma
-// myFunction.apply(myObj, ['ABC', 'DEF']); // a for apply and array
-
-/* Arrow Function and Lexical scope */
-// class MyClass2{
-//   myMethod() {
-//     const foo = 123;
-//     console.log('1', this);
-//     setTimeout(() => {
-//       console.log('2', this); // scope of this statements
-//     }, 0); 
-//   }
-//   foo() {
-//     const foo = 456; // different scope than previous foo
-//   }
-// }
-// const MyInstance2 = new MyClass2;
-// MyInstance2.myMethod();
-
-/* Typing "this" and "noImplicitThis" */
-const elem = document.querySelector('.click');
-
-function handleClick(this: HTMLAnchorElement, event: Event) {
-  event.preventDefault();
-  console.log(this.className);
+const anotherPerson: Person = {
+  name: 'John',
+  age: 30
 }
 
-elem.addEventListener('click', handleClick, false);
+const anotherOtherPerson: typeof person = {
+  name: 'name 3',
+  age: 3
+}
+// JavaScript
+// typeof person = 'object'
