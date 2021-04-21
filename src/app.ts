4ee0708a91c1de93eb0832afbf02a0b8231b1626
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-04-21 15:13:01 -0700

    typing this and noImplicitThis

--- src/app.ts
+++ src/app.ts
@@ -27,31 +27,41 @@ const myObj = {
 
 /* Exploring “this” with .call, .apply and .bind */
 // continued
-function myFunction(...text: string[]) {
-  console.log('Function:::', this, text);
-}
+// function myFunction(...text: string[]) {
+//   console.log('Function:::', this, text);
+// }
 
 // myFunction('ABC', 'DEF');
-const bindFunction = myFunction.bind(myObj);
-// bind will take initial function, bind context to it, then give it a new context on call
-bindFunction('ABC', 'DEF');
-bindFunction('123','456');
-bindFunction('ABC', 'DEF');
-myFunction.call(myObj, 'ABC', 'DEF'); // c for call and comma
-myFunction.apply(myObj, ['ABC', 'DEF']); // a for apply and array
+// const bindFunction = myFunction.bind(myObj);
+// // bind will take initial function, bind context to it, then give it a new context on call
+// bindFunction('ABC', 'DEF');
+// bindFunction('123','456');
+// bindFunction('ABC', 'DEF');
+// myFunction.call(myObj, 'ABC', 'DEF'); // c for call and comma
+// myFunction.apply(myObj, ['ABC', 'DEF']); // a for apply and array
 
 /* Arrow Function and Lexical scope */
-class MyClass2{
-  myMethod() {
-    const foo = 123;
-    console.log('1', this);
-    setTimeout(() => {
-      console.log('2', this); // scope of this statements
-    }, 0); 
-  }
-  foo() {
-    const foo = 456; // different scope than previous foo
-  }
+// class MyClass2{
+//   myMethod() {
+//     const foo = 123;
+//     console.log('1', this);
+//     setTimeout(() => {
+//       console.log('2', this); // scope of this statements
+//     }, 0); 
+//   }
+//   foo() {
+//     const foo = 456; // different scope than previous foo
+//   }
+// }
+// const MyInstance2 = new MyClass2;
+// MyInstance2.myMethod();
+
+/* Typing "this" and "noImplicitThis" */
+const elem = document.querySelector('.click');
+
+function handleClick(this: HTMLAnchorElement, event: Event) {
+  event.preventDefault();
+  console.log(this.className);
 }
-const MyInstance2 = new MyClass2;
-MyInstance2.myMethod();
+
+elem.addEventListener('click', handleClick, false);
