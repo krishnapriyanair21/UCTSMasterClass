e27d330848534ad3b6f99f14d6343ec94aed3835
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-04-21 12:41:08 -0700

    .call, .apply .bind

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 /* A “this” Keyword Primer */
 
 // function
-function myFunction() {
-  console.log('Function:::', this);
-}
+// function myFunction() {
+//   console.log('Function:::', this);
+// }
 
-myFunction();
+// myFunction();
 
 // Object literal
 const myObj = {
@@ -13,13 +13,29 @@ const myObj = {
     console.log('Object::::', this);
   }
 }
-myObj.myMethod();
+// myObj.myMethod();
+
 // Classes
-class MyClass{
-  MyMethod() {
-    console.log('Class::::', this);
-  }
+// class MyClass{
+//   MyMethod() {
+//     console.log('Class::::', this);
+//   }
+// }
+
+// const MyInstance = new MyClass();
+// MyInstance.MyMethod();
+
+/* Exploring “this” with .call, .apply and .bind */
+// continued
+function myFunction(...text: string[]) {
+  console.log('Function:::', this, text);
 }
 
-const MyInstance = new MyClass();
-MyInstance.MyMethod();
+// myFunction('ABC', 'DEF');
+const bindFunction = myFunction.bind(myObj);
+// bind will take initial function, bind context to it, then give it a new context on call
+bindFunction('ABC', 'DEF');
+bindFunction('123','456');
+bindFunction('ABC', 'DEF');
+myFunction.call(myObj, 'ABC', 'DEF'); // c for call and comma
+myFunction.apply(myObj, ['ABC', 'DEF']); // a for apply and array
