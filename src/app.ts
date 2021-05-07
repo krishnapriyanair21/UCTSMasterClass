9fdecf1a7fe87bc99a3e1c0aace50e6064ae3a51
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-05-06 20:53:14 -0700

    discriminated unions

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,59 @@
 /* Intersection Types */
 
-interface Order{
+// interface Order{
+//   id: string;
+//   amount: number;
+//   currency: string;
+// }
+
+// interface Stripe{
+//   card: string;
+//   cvc: string;
+// }
+
+// interface PayPal{
+//   email: string;
+// }
+//  // keeps interfaces separate
+// type CheckoutCard = Order & Stripe;
+// type CheckoutPayPal = Order & PayPal;
+
+// const order: Order = {
+//   id: 'xj28s',
+//   amount: 100,
+//   currency: 'USD'
+// };
+
+// const orderCard: CheckoutCard = {
+//   ...order,
+//   card: '1000 2000 3000 4000',
+//   cvc:'123'
+// };
+
+// const orderPayPal: CheckoutPayPal = {
+//   ...order,
+//   email: 'abc@def.com'
+// };
+
+// const assigned = Object.assign({}, order, orderCard);
+
+/* Discriminated (Tagged) Unions */
+ // continued
+
+ interface Order{
   id: string;
   amount: number;
   currency: string;
 }
 
 interface Stripe{
+  type: 'stripe';
   card: string;
   cvc: string;
 }
 
 interface PayPal{
+  type: 'paypal';
   email: string;
 }
  // keeps interfaces separate
@@ -26,13 +68,23 @@ const order: Order = {
 
 const orderCard: CheckoutCard = {
   ...order,
+  type: 'stripe',
   card: '1000 2000 3000 4000',
   cvc:'123'
 };
 
 const orderPayPal: CheckoutPayPal = {
   ...order,
+  type:'paypal',
   email: 'abc@def.com'
 };
 
-const assigned = Object.assign({}, order, orderCard);
+type Payload = CheckoutCard | CheckoutPayPal;
+function checkout(payload: Payload) {
+  if (payload.type === 'stripe') {
+    console.log(payload.card, payload.cvc);
+  }
+  if (payload.type === 'paypal') {
+    console.log(payload.email);
+  }
+}
