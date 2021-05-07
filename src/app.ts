7268da4ee89c9bf971bf04e6ae17a90ccb0c0ee9
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-05-07 12:36:36 -0700

    string enums

--- src/app.ts
+++ src/app.ts
@@ -14,3 +14,18 @@ const selectedSize = 2;
 console.log(Sizes.Medium);
 console.log(Sizes.Large, Sizes[Sizes.Large]);
 console.log(Sizes[selectedSize]);
+
+/* String Enums and Inlining Members */
+const enum Sizes2{ // inline members
+  Small = 'small',
+  Medium = 'medium',
+  Large = 'large',
+}
+
+let selected: Sizes2 = Sizes2.Small;
+
+function updateSize(size: Sizes2): void{
+  selected = size;
+}
+
+updateSize(Sizes2.Large);
