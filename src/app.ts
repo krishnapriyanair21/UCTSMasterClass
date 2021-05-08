d3ebb4a6a46e5d4e9dec083be9456b148bc96cb4
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-05-07 18:33:51 -0700

    Augementing Modules with Declaration Files

--- src/app.ts
+++ src/app.ts
@@ -5,3 +5,12 @@ import * as _ from "lodash";
 
 _.chunk([1, 2, 3, 4], 2); // [1,2],[3,4]
 
+/* Augmenting Modules with Declarations */
+_.mixin({
+  log(item:string) {
+    console.log(':::', item);
+  },
+})
+
+// built ins show up with _. 
+_.log('Hello')
