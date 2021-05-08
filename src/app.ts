f932cfa29911b12c9b62146773060b8afd6d730f
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-05-07 18:21:35 -0700

    writing declaration files

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,7 @@
-/* Numeric Enums and Reverse Mappings */
+import * as _ from "lodash";
 
-enum Sizes{
-  Small,
-  Medium,
-  Large
-}
-enum Sizes{
-  ExtraLarge = 3,
-}
 
-const selectedSize = 2;
+/* Writing Declaration Files */
 
-console.log(Sizes.Medium);
-console.log(Sizes.Large, Sizes[Sizes.Large]);
-console.log(Sizes[selectedSize]);
+_.chunk([1, 2, 3, 4], 2); // [1,2],[3,4]
 
-/* String Enums and Inlining Members */
-const enum Sizes2{ // inline members
-  Small = 'small',
-  Medium = 'medium',
-  Large = 'large',
-}
-
-let selected: Sizes2 = Sizes2.Small;
-
-function updateSize(size: Sizes2): void{
-  selected = size;
-}
-
-updateSize(Sizes2.Large);
