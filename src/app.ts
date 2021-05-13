7aff9319245d57585125e19f2db1a1c4a6012ce1
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-05-13 11:35:12 -0700

    include, exclude, and files

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,4 @@
-// import * as _ from "lodash";
+/* Include, Exclude and Files properties */
 
-
-// /* Writing Declaration Files */
-
-// _.chunk([1, 2, 3, 4], 2); // [1,2],[3,4]
-
-// /* Augmenting Modules with Declarations */
-// _.mixin({
-//   log(item:string) {
-//     console.log(':::', item);
-//   },
-// })
-
-// // built ins show up with _. 
-// _.log('Hello')
-
-/* Emitting Declaration Files */
-export class Foo {
-  constructor(public name: string) { }
-  bar(age: number) { }
-}
+// all code in tsconfig
+// precedent is files -> include -> exclude
