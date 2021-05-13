f1928d2dbeb5caeac3b84f3acfc616505d55cba6
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-05-13 11:09:33 -0700

    Emitting Declaration Files

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,22 @@
-import * as _ from "lodash";
+// import * as _ from "lodash";
 
 
-/* Writing Declaration Files */
+// /* Writing Declaration Files */
 
-_.chunk([1, 2, 3, 4], 2); // [1,2],[3,4]
+// _.chunk([1, 2, 3, 4], 2); // [1,2],[3,4]
 
-/* Augmenting Modules with Declarations */
-_.mixin({
-  log(item:string) {
-    console.log(':::', item);
-  },
-})
+// /* Augmenting Modules with Declarations */
+// _.mixin({
+//   log(item:string) {
+//     console.log(':::', item);
+//   },
+// })
 
-// built ins show up with _. 
-_.log('Hello')
+// // built ins show up with _. 
+// _.log('Hello')
+
+/* Emitting Declaration Files */
+export class Foo {
+  constructor(public name: string) { }
+  bar(age: number) { }
+}
