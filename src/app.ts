8a74e5d947ff7fb4eee52842567d6d36043cc4b8
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-05-18 11:47:32 -0700

    Output Location

--- src/app.ts
+++ src/app.ts
@@ -6,4 +6,10 @@
 /* Configuration Inheritance with Extends */
 //inherit from a different tsconfig file
 // change tsconfig to tsconfig-base
-function log(message: string) { }
+// function log(message: string) { }
+
+
+/* Output Locations, Tweaks and Emitting On Error */
+//outDir
+//outFile
+console.log('app')
